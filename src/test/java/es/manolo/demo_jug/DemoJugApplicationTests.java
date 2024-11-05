6e14ecdd5d7b897775c4da220d96de79bcc08623
Manolo Carrasco <manolo@vaadin.com>
2024-11-05 07:07:59 +0100

    1. project downloaded from spring initializr

---
+++ src/test/java/es/manolo/demo_jug/DemoJugApplicationTests.java
@@ -0,0 +1,13 @@
+package es.manolo.demo_jug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
