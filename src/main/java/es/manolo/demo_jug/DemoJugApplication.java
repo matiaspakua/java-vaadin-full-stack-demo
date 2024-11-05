dc69fc63d77411bf5a5fde1f0c5c332c9a9fbcd2
Manolo Carrasco <manolo@vaadin.com>
2024-11-05 07:07:59 +0100

    1. project downloaded from spring initializr

---
+++ src/main/java/es/manolo/demo_jug/DemoJugApplication.java
@@ -0,0 +1,13 @@
+package es.manolo.demo_jug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJugApplication.class, args);
+	}
+
+}
